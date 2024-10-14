9e66b5f041a3d800a053b967c54da30b0566e603
Oscar <nwanzeoscar@gmail.com>
2024-10-14 15:11:55 +0100

    minor fixes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route exact path="/" element={<InstitutionsPage />} />
           <Route
             exact
             path="/api/institution-mgmt/institutions"
